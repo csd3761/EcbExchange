00a2b170737a8d28a51c85e5077bcaf05185d6db
csd3761 <csd3761@csd.uoc.gr>
2025-01-23 11:16:42 +0200

    commit random

--- ECB.Infrastructure/Cache/Redis/RedisCacheService.cs
+++ ECB.Infrastructure/Cache/Redis/RedisCacheService.cs
@@ -12,7 +12,7 @@ public class RedisCacheService : ICacheService
         _database = database;
     }
 
-    // this is a random commenttt
+    // this is a random commenttttt
     public Task<T?> GetAsync<T>(string key)
     {
         throw new NotImplementedException();
